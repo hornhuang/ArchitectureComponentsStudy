3d5cd569310648ad41c5cca3864d02a1edf2dcd2
huangyuanhao <307975180@qq.com>
2019-09-15 15:18:55 +0800

    Build My Ui Design

--- basicrxjavasample/src/main/java/com/entry/basicrxjavasample/ui/UserActivity.java
+++ basicrxjavasample/src/main/java/com/entry/basicrxjavasample/ui/UserActivity.java
@@ -1,8 +1,10 @@
-package com.entry.basicrxjavasample;
+package com.entry.basicrxjavasample.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.entry.basicrxjavasample.R;
 
 public class UserActivity extends AppCompatActivity {
 
