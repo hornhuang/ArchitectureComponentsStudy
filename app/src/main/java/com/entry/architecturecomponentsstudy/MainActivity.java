9f485e20cb85c4e352c3f7f60736b12783796b4c
huangyuanhao <307975180@qq.com>
2019-09-15 15:14:32 +0800

    Project  Build and Study Begin.

---
+++ app/src/main/java/com/entry/architecturecomponentsstudy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.entry.architecturecomponentsstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
