a888049cf82428dcf4ecb25b0eac6be0d6a24d3b
huangyuanhao <307975180@qq.com>
2019-09-17 20:49:03 +0800

    实现 User 表的数据功能

--- basicrxjavasample/src/main/java/com/entry/basicrxjavasample/ui/UserViewModel.java
+++ basicrxjavasample/src/main/java/com/entry/basicrxjavasample/ui/UserViewModel.java
@@ -2,8 +2,41 @@ package com.entry.basicrxjavasample.ui;
 
 import androidx.lifecycle.ViewModel;
 
+import com.entry.basicrxjavasample.UserDataSource;
+import com.entry.basicrxjavasample.persistance.User;
+
+import io.reactivex.Flowable;
+import io.reactivex.functions.Function;
+
+/**
+ * UserActivity 的 ViewModel
+ * 作用：
+ */
 public class UserViewModel extends ViewModel {
 
+    /**
+     * UserDataSource 接口
+     */
+    private final UserDataSource mDataSource;
 
+    private User mUser;
+
+    public UserViewModel(UserDataSource dataSource){
+        this.mDataSource = dataSource;
+    }
+
+    /**
+     * 从数据库中读取
+     * @return
+     */
+    public Flowable<String> getUserName(){
+        return mDataSource.getUser()
+                .map(new Function<User, String>() {
+                    @Override
+                    public String apply(User user) throws Exception {
+                        return user.getUserName();
+                    }
+                });
+    }
 
 }
